4871fcec7bbeda7d0789421864624314accd07a7
Denis Defreyne <denis.defreyne@stoneship.org>
2015-06-27 10:24:04 +0200

    Replace contents with pointer to new nanoc 4

--- NEWS.md
+++
@@ -1,5 +0,0 @@
-# nanoc-relativize_paths news
-
-## 1.0.0
-
-Initial release, transplanted from nanoc 3.6.3.
--- lib/nanoc/relativize_paths.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-module Nanoc
-  module RelativizePaths
-  end
-end
-
-require 'nanoc/relativize_paths/version'
-require 'nanoc/relativize_paths/filter'
